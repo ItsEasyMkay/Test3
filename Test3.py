ec3534ed10ad39527a58be22b79b757e2387709d
ItsEasyMkay <60890163+ItsEasyMkay@users.noreply.github.com>
2020-03-09 15:32:29 +0000

    Add files via upload

---
+++ Test3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+num1 = int(input())
+num2 = int(input())
+num3 = 0
+
+for i in range (num1):
+        num3 = num3 + num2
+        
+print(num3)
+        
+        
